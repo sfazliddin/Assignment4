43252c9a5e2db9370756edcd9e8dd156135d0e7e
Smqkmj <gotenks.ssj499@gmail.com>
2022-01-07 09:34:54 -0500

    finished bonus question

---
+++ BONUS/BONUS.js
@@ -0,0 +1,16 @@
+const volume = document.createElement('p')
+const cont = document.querySelector(".container")
+volume.textContent = "5"
+console.log(volume.textContent)
+
+let getVolume = () => {
+    let radius = document.forms["findingVolume"]["radiusInput"].value
+    volume.textContent = "Volume of Sphere= "+4 * Math.PI * Math.pow(radius, 3) / 3
+    console.log("radius= ",radius)
+    console.log("volume=",volume)
+    cont.appendChild(volume)
+    return false
+
+
+
+}
