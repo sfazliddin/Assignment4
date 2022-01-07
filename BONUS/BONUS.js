ad53037fdef5693c4c19ae134465140f67291b80
Smqkmj <gotenks.ssj499@gmail.com>
2022-01-07 09:36:19 -0500

    formatted all files

--- BONUS/BONUS.js
+++ BONUS/BONUS.js
@@ -5,9 +5,9 @@ console.log(volume.textContent)
 
 let getVolume = () => {
     let radius = document.forms["findingVolume"]["radiusInput"].value
-    volume.textContent = "Volume of Sphere= "+4 * Math.PI * Math.pow(radius, 3) / 3
-    console.log("radius= ",radius)
-    console.log("volume=",volume)
+    volume.textContent = "Volume of Sphere= " + 4 * Math.PI * Math.pow(radius, 3) / 3
+    console.log("radius= ", radius)
+    console.log("volume=", volume)
     cont.appendChild(volume)
     return false
 
--- ImRight/ImRightButton.js
+++ ImRight/ImRightButton.js
@@ -1,11 +1,11 @@
 
 const whoIsCorrect = document.createElement("p")
 const divContainer = document.querySelector("div")
-let Iam=()=>{
-    whoIsCorrect.innerText="I'm right!!"
+let Iam = () => {
+    whoIsCorrect.innerText = "I'm right!!"
 }
-let nope=()=>{
-    whoIsCorrect.innerText="No, I'm right!!!"
+let nope = () => {
+    whoIsCorrect.innerText = "No, I'm right!!!"
 }
 divContainer.append(whoIsCorrect)
 
